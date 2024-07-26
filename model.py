db3f87f6d99717767999b4bcc4a5d51454b5f771
toni-akintola <oakintol@nd.edu>
2024-07-26 13:09:40 -0400

    fixing zollman bandit

--- model.py
+++ model.py
@@ -1,101 +1,58 @@
-import networkx as nx
-import numpy as np
 import random
+import numpy as np
 from modelpy_abm.main import AgentModel
 
 
 def constructModel() -> AgentModel:
-    def generateInitialData(model: AgentModel):
+    def generateInitialData():
         initial_data = {
-            # bandit arm A is set to a 0.5 success rate in the decision process
-            "a_success_rate": 0.5,
-            # bandit arm B is a learned parameter for the agent. Initialize randomly
-            "b_success_rate": random.uniform(0.01, 0.99),
-            # agent evidence learned, will be used to update their belief and others in the network
-            "b_evidence": None,
-            # population type, 'marginalized' or 'dominant'
-            "type": (
-                "dominant"
-                if random.random() > model.proportion_marginalized
-                else "marginalized"
-            ),
+            "a_alpha": random.randint(1, 4),
+            "a_beta": random.randint(1, 4),
+            "b_alpha": random.randint(1, 4),
+            "b_beta": random.randint(1, 4),
         }
+        expectations = {
+            "a_expectation": initial_data["a_alpha"]
+            / (initial_data["a_alpha"] + initial_data["a_beta"]),
+            "b_expectation": initial_data["b_alpha"]
+            / (initial_data["b_alpha"] + initial_data["b_beta"]),
+        }
+        initial_data.update(expectations)
+        return initial_data
 
     def generateTimestepData(model: AgentModel):
         graph = model.get_graph()
-        # run the experiments in all the nodes
+
         for _node, node_data in graph.nodes(data=True):
             # agent pulls the "a" bandit arm
-            if node_data["a_success_rate"] > node_data["b_success_rate"]:
-                # agent won't have any new evidence gathered for b
-                node_data["b_evidence"] = None
+            if node_data["a_expectation"] > node_data["b_expectation"]:
+                node_data["a_alpha"] += int(
+                    np.random.binomial(
+                        model["num_trials"], model["a_objective"], size=None
+                    )
+                )
+                node_data["a_beta"] += model["num_trials"] - node_data["a_alpha"]
+                node_data["a_expectation"] = node_data["a_alpha"] / (
+                    node_data["a_alpha"] + node_data["a_beta"]
+                )
 
             # agent pulls the "b" bandit arm
             else:
-                # agent collects evidence
-                node_data["b_evidence"] = int(
+                node_data["b_alpha"] += int(
                     np.random.binomial(
-                        model["num_pulls"], model["objective_b"], size=None
+                        model["num_trials"], model["b_objective"], size=None
                     )
                 )
-
-        # define function to calculate posterior belief
-        def calculate_posterior(prior_belief: float, num_evidence: float) -> float:
-            # Calculate likelihood, will be either the success rate
-            pEH_likelihood = (model["objective_b"] ** num_evidence) * (
-                (1 - model["objective_b"]) ** (model["num_pulls"] - num_evidence)
-            )
-
-            # Calculate normalization constant
-            pE_evidence = (pEH_likelihood * prior_belief) + (
-                (1 - model["objective_b"]) ** num_evidence
-            ) * (model["objective_b"] ** (model["num_pulls"] - num_evidence)) * (
-                1 - prior_belief
-            )
-
-            # Calculate posterior belief using Bayes' theorem
-            posterior = (pEH_likelihood * prior_belief) / pE_evidence
-
-            return posterior
-
-        # update the beliefs, based on evidence and neighbors
-        for node, node_data in graph.nodes(data=True):
-            neighbors = graph.neighbors(node)
-            # update belief of "b" on own evidence gathered
-            if node_data["b_evidence"] is not None:
-                node_data["b_success_rate"] = calculate_posterior(
-                    node_data["b_success_rate"], node_data["b_evidence"]
+                node_data["b_beta"] += model["num_trials"] - node_data["b_alpha"]
+                node_data["b_expectation"] = node_data["b_alpha"] / (
+                    node_data["b_alpha"] + node_data["b_beta"]
                 )
 
-            # update node belief of "b" based on evidence gathered by neighbors
-            for neighbor_node in neighbors:
-                neighbor_evidence = graph.nodes[neighbor_node]["b_evidence"]
-                neighbor_type = graph.nodes[neighbor_node]["type"]
+        model.set_graph(graph)
 
-                # update from all neighbors if current node is marginalized
-                if (
-                    node_data["type"] == "marginalized"
-                    and neighbor_evidence is not None
-                ):
-                    node_data["b_success_rate"] = calculate_posterior(
-                        node_data["b_success_rate"], neighbor_evidence
-                    )
-
-                # only update from dominant agents if current node is dominant
-                elif neighbor_type != "marginalized" and neighbor_evidence is not None:
-                    node_data["b_success_rate"] = calculate_posterior(
-                        node_data["b_success_rate"], neighbor_evidence
-                    )
-
+    # Recreating the Zollman Model
     model = AgentModel()
-    model.update_parameters(
-        {
-            "num_agents": 3,
-            "proportion_marginalized": 1 / 6,
-            "num_pulls": 1,
-            "objective_b": 0.51,
-        }
-    )
+    model.update_parameters({"a_objective": 0.49, "b_objective": 0.51, "num_trials": 1})
 
     model.set_initial_data_function(generateInitialData)
     model.set_timestep_function(generateTimestepData)
