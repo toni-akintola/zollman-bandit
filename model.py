a657f39bca6daecb6e97ec35c182b1c4dc4d3148
toni-akintola <oakintol@nd.edu>
2024-08-02 11:15:38 -0400

    added .ipynb

--- model.py
+++ model.py
@@ -29,7 +29,8 @@ def generateTimestepData(model: AgentModel):
             node_data["a_alpha"] += int(
                 np.random.binomial(model["num_trials"], model["a_objective"], size=None)
             )
-            node_data["a_beta"] += model["num_trials"] - node_data["a_alpha"]
+            # TODO
+            node_data["a_beta"] += max(0, model["num_trials"] - node_data["a_alpha"])
             node_data["a_expectation"] = node_data["a_alpha"] / (
                 node_data["a_alpha"] + node_data["a_beta"]
             )
@@ -39,7 +40,8 @@ def generateTimestepData(model: AgentModel):
             node_data["b_alpha"] += int(
                 np.random.binomial(model["num_trials"], model["b_objective"], size=None)
             )
-            node_data["b_beta"] += model["num_trials"] - node_data["b_alpha"]
+            # TODO
+            node_data["b_beta"] += max(0, model["num_trials"] - node_data["b_alpha"])
             node_data["b_expectation"] = node_data["b_alpha"] / (
                 node_data["b_alpha"] + node_data["b_beta"]
             )
@@ -50,9 +52,24 @@ def generateTimestepData(model: AgentModel):
 def constructModel() -> AgentModel:
     # Recreating the Zollman Model
     model = AgentModel()
-    model.update_parameters({"a_objective": 0.49, "b_objective": 0.51, "num_trials": 1})
+    model.update_parameters({"a_objective": 0.19, "b_objective": 0.71, "num_trials": 5})
 
     model.set_initial_data_function(generateInitialData)
     model.set_timestep_function(generateTimestepData)
 
     return model
+
+
+model = constructModel()
+model["num_nodes"] = 20
+model.initialize_graph()
+
+timesteps = 20
+
+for _ in range(timesteps):
+    model.timestep()
+
+graph = model.get_graph()
+
+for node, node_data in graph.nodes(data=True):
+    print(node_data["a_expectation"], node_data["b_expectation"])
